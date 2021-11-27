07c8129b2026b906f348375b36af7447e95981d7
Ryan_Bunn <rrbunn2@ncsu.edu>
2021-11-27 15:41:40 -0500

    Commit 2, completed about page and added skeletal structure for data exploration.

--- Project3/server.R
+++ Project3/server.R
@@ -2,13 +2,44 @@
 
 library(shiny)
 library(tidyverse)
+library(ggplot2)
 
-shinyServer(function(input, output) {
-  data<-read_csv("../africa_recession.csv") 
-  output$Plot <- renderPlot({
-    
-    hist(data$pop)
-    
+shinyServer(function(input, output,session) {
+  #data set
+  temp<-read_csv("../africa_recession.csv")[,1:5]
+  data <- isolate(temp)
+  #about objects
+  output$about <- renderText({
+    c("The purpose of this application is to compare 3 seperate supervised learning models for the African Recession Data. The data comes from (https://www.kaggle.com/chirin/african-country-recession-dataset-2000-to-2017?select=africa_recession.csv) and contains various indicators of economic preformance for various african countries from 2000 to 2017. The Data Exploration tab allows the user to create summaries, graphs and tables. The Modeling tab shows 3 models,  a multiple linear regression model, regression tree, and a random forest model.The Data tab contains the dataset we used in the analysis")
   })
+  output$image <- renderImage({
+    list(src ="../AfricaRecessionDatasetDescription.PNG" )
+  })
+  
+  #data exploration objects
+  observeEvent(input$type,
+  if(input$type == "g"){
+  observeEvent(input$go1,  
+  output$Plot <- renderPlot({
+    if(input$go1 == "histogram"){
+    ggplot(data=data)+geom_histogram(aes_string(input$var))
+    
+  }else if(input$go1 == "scatter"){
+    
+    ggplot(data=data)+geom_point(aes_string(input$var,input$var.int))
+    
+  }else{
+    if(input$var.box == "none"){
+      ggplot(data=data)+geom_boxplot(aes_string(input$var))}
+    else{
+      ggplot(data=data)+geom_boxplot(aes_string(input$var,input$var.box))
+    }
+  }
+    })
+  )
+    })
+  #Modeling objects
+  
+  #Data objects
   
 })
--- Project3/ui.R
+++ Project3/ui.R
@@ -8,15 +8,36 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("Project 3"),
   tabsetPanel(
-      tabPanel("About"),
+    #about tab
+      tabPanel("About",
+               textOutput("about"),
+               imageOutput("image")
+               ),
+      
+      #data exploration tab
       tabPanel("Data Exploration",
+               #conditional panels for responses and graphs/tables of interest
+               selectInput("var",label = "Variable",choices=list("pop"="pop","emp"="emp","hc"="hc","ccon"="ccon")),
+               selectInput("type","What would you like to see?",choices=list("Graph"="g","Table"="t")),
+               conditionalPanel("input.type == 't'",selectInput("to1","Limiting Variable",list("none","option2"))),
+               conditionalPanel("input.type == 'g'",selectInput("go1","Which type of graph",
+                  list("scatter","histogram","boxplot")), conditionalPanel("input.go1 == 'scatter'",
+                    selectInput("var.int","y variable of interest",choices=list("pop"="pop","emp"="emp","hc"="hc","ccon"="ccon"))),
+                  conditionalPanel("input.go1 == 'boxplot'",
+                    selectInput("var.box","grouping variable",choices=list("none","op1")))),
+               
+               #plot based upon input                 
                plotOutput("Plot")),
+      
+      #modeling tab
       tabPanel("Modeling",
                tabsetPanel(
                  tabPanel("Modeling Info"),
                  tabPanel("Model Fitting"),
                  tabPanel("Prediction")
                )),
+      
+      #data tab
       tabPanel("Data")
     )
   )
